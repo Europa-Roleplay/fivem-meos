53fde0978cd9ae1b5ebb514d9d4a5670308e1651
Senna <62171904+Poseidon281@users.noreply.github.com>
2025-04-17 19:52:18 +0200

    Squashed commit of the following:
    commit f67e87eedef616a3de47b1037d9f04abc92e7a66
    Author: MilanNuis <57843223+MilanNuis@users.noreply.github.com>
    Date:   Thu Apr 17 17:51:56 2025 +0000
    
        Pint
    
    commit cec53d4e676debaa8160939c8e2898ca8bac2654
    Author: Milan Nuis <milannuis@gmail.com>
    Date:   Thu Apr 17 19:51:29 2025 +0200
    
        fixes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\LogboekController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\CitizenController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\NoteController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\OfficerNotesController;
 use App\Http\Controllers\ProfileController;
@@ -23,28 +20,16 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::controller(AdminController::class)->name('admin')->prefix('admin')->group(function () {
-        Route::get('/', 'index')->name('.index');
+Route::prefix('admin')->name('admin')->group(function () {
+    Route::controller(AdminController::class)->group(function () {
+        Route::get(null, 'index')->name('.index');
         Route::prefix('gebruikers')->name('.users')->group(function () {
-            Route::get('/', 'users');
-            Route::post('/', 'store')->name('.store');
+            Route::get(null, 'users');
+            Route::post(null, 'store')->name('.store');
             Route::post('/{user}', 'update')->name('.update');
             Route::delete('/{user}', 'destroy')->name('.destroy');
             Route::post('/status/{user}', 'status')->name('.status');
             Route::get('/dossier/{user}', 'dossier')->name('.dossier');
-        });
-
-        Route::controller(TrainingController::class)->name('.trainingen')->prefix('trainingen')->group(function () {
-            Route::get('/', 'index');
-            Route::post('/', 'store')->name('.store');
-            Route::post('/{training}', 'update')->name('.update');
-            Route::delete('/{training}', 'destroy')->name('.destroy');
-        });
-
-        Route::controller(LogboekController::class)->name('.logboek')->prefix('logboek')->group(function () {
-            Route::get('/', 'index');
-            Route::get('/export', 'export')->name('.export');
         });
 
         Route::controller(TrainingController::class)->name('.trainingen')->prefix('trainingen')->group(function () {
@@ -80,9 +65,6 @@ Route::middleware('auth')->group(function () {
             Route::get(null, 'index');
             Route::post('/', 'search')->name('.search');
             Route::get('/{id}', 'show')->name('.show');
-            // Route::get('/{citizen}', 'show')->name('.show');
-            // Route::post('/{citizen}', 'update')->name('.update');
-            // Route::delete('/{citizen}', 'destroy')->name('.destroy');
         });
     });
 });
