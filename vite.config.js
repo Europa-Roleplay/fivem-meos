7e3dc1e67243294b1fd5aa40e9a1332e65e18c37
Senna <sennavaniterson1@gmail.com>
2025-04-17 17:29:52 +0000

    Fix localhost vite config

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server: {
-        host: true,
-        port: 5173,
-        hmr: {
-            host: 'localhost',
-            port: 5173
-        },
-    },
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
